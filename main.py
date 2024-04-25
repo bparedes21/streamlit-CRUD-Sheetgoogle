06077253cb33afea1cc9b4c467b0a2d38820870b
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-25 17:51:58 -0300

    empty

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import requests
 import streamlit as st
 import pandas as pd
+
 # URL de tu API de FastAPI
 api_url = "https://python-fastapi-iamgod.koyeb.app"  # Actualiza con la URL de tu API
 
 # Función para obtener los datos de la API
 def get_data():
     try:
-        response = requests.get(f"https://python-fastapi-iamgod.koyeb.app/read")
+        response = requests.get(f"{api_url}/read")
         data = response.json()
 
         if response.status_code == 200:
-            data =data[0]
-            list_data=data['data']
-            df=pd.DataFrame(list_data)
+            data = data[0]
+            list_data = data['data']
+            df = pd.DataFrame(list_data)
             return df
         else:
             st.error(data)
@@ -23,7 +24,11 @@ def get_data():
         return None
 
 # Obtener los datos de la API
-#data = get_data()
+data = get_data()
 
-st.title("Datos de la API CRUD Google Sheets")
-#st.write(data)
+# Mostrar los datos en Streamlit
+if data is not None and not data.empty:
+    st.title("Datos de la API CRUD Google Sheets")
+    st.write(data)
+else:
+    st.warning("No se encontraron datos.")
