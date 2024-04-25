dbf53e9cb72807799e1ed5c0fc869b68f88a02f7
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-25 20:09:32 -0300

    empty

--- main.py
+++ main.py
@@ -74,9 +74,10 @@ if selected_opcion_crud == 'modificar':
         if st.button("Modificar"):
             response = update_data(selected_id_list, selected_category, selected_productos, precio, selected_descuento)
             if response.status_code == 200:
-                st.success("Datos modificados exitosamente")
+                
                 st.empty()
                 data = get_data()
+                st.success("Datos modificados exitosamente")
             else:
                 st.error(f"Hubo un error al modificar los datos")
 
