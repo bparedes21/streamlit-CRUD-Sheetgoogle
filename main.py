9621079a3dd6023554c829363b07bcb3180e803a
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-25 17:33:32 -0300

    streaplit app

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
 import streamlit as st
-
+import pandas as pd
 # URL de tu API de FastAPI
 api_url = "https://python-fastapi-iamgod.koyeb.app"  # Actualiza con la URL de tu API
 
 # Función para obtener los datos de la API
 def get_data():
     try:
-        response = requests.get(f"{api_url}/read")
+        response = requests.get(f"https://python-fastapi-iamgod.koyeb.app/read")
         data = response.json()
+
         if response.status_code == 200:
-            data = response.json()["data"]
-            return data
+            data =data[0]
+            list_data=data['data']
+            df=pd.DataFrame(list_data)
+            return df
         else:
             st.error(data)
             return None
