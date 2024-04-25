383c036828fa2f8f6a6550942943258133aeee90
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-25 19:31:38 -0300

    opcion modificar

--- main.py
+++ main.py
@@ -23,6 +23,19 @@ def get_data():
         st.error(f"Error al conectar con la API: {e}")
         return None
 
+
+# Función para realizar la solicitud POST al endpoint de actualización
+def update_data(id_value, category, product, price, discount):
+    url = "https://python-fastapi-iamgod.koyeb.app/update/" + id_value
+    payload = {
+        "column1": category,
+        "column2": product,
+        "column3": price,
+        "column4": discount
+    }
+    response = requests.put(url, json=payload)
+    return response
+
 # Obtener los datos de la API
 data = get_data()
 
@@ -38,7 +51,7 @@ if selected_opcion_crud == 'modificar':
     # Obtener la lista de valores de la columna "ID"
     id_list = data['ID'].tolist()
     if len(id_list)!=0:
-
+        selected_id_list= st.selectbox("Selecciona un ID de la tabla Productos:", id_list)    
         # Obtener las categorías
         categories = ['Almacen','Mascotas','Bebidas y bodega']
         productos_Almacen = ['Fideos','Arroz','Pure de Tomate']
@@ -49,21 +62,26 @@ if selected_opcion_crud == 'modificar':
         # Crear menú desplegable con las categorías
         selected_category = st.selectbox("Selecciona una categoría:", categories)
         if selected_category == 'Almacen':
-            selected_productos_Almacen = st.selectbox("Seleccione un producto:", productos_Almacen)
+            selected_productos = st.selectbox("Seleccione un producto:", productos_Almacen)
         elif selected_category == 'Mascotas':
-            selected_productos_Mascotas = st.selectbox("Seleccione un producto:", productos_Mascotas)
+            selected_productos = st.selectbox("Seleccione un producto:", productos_Mascotas)
         elif selected_category == 'Bebidas y bodega':
-            selected_productos_Bebidas_y_bodega = st.selectbox("Seleccione un producto:", productos_Bebidas_y_bodega)
-
+            selected_productos = st.selectbox("Seleccione un producto:", productos_Bebidas_y_bodega)
+        descuento = ['0','10','20','30']
+        selected_descuento = st.selectbox("Seleccione un descuento:", descuento)
+        precio = st.text_input("Ingresar precio:")
 
         if st.button("Modificar"):
-            # Aquí puedes realizar la lógica para modificar los datos en la API
-            st.success("Datos modificados exitosamente")
+            response = update_data(selected_id_list, selected_category, selected_productos, precio, selected_descuento)
+            if response.status_code == 200:
+                st.success("Datos modificados exitosamente")
+            else:
+                st.error(f"Hubo un error al modificar los datos: {response.json()['message']}")
+        
     else:
 
-        # Aquí puedes realizar la lógica para modificar los datos en la API
-            st.success("No hay nungun registro para modificar")
-        
+        st.warning("No se encontraron IDs para la categoría seleccionada.")
+
 # Mostrar los datos en Streamlit
 if data is not None and not data.empty:
     st.title("Datos de la API CRUD Google Sheets")
