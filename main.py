5ae2da07c27b62892251639af35a09969b3f8499
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 14:41:22 -0300

    menu false

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
+# Set page configuration
+st.set_page_config(
+    page_title="Gestion de Hoja de Calculo",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    show_sidebar_navigation='False'
+)
+
+# Dictionary with page names and URLs
+pages = {
+    "Borrar": "/borrar",
+    "Insertar": "/insertar",
+    "Modificar": "/modificar"
+}
+
+# Display navigation menu
 st.sidebar.title("Navegación")
