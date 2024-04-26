4827e3fd5242662775f5c03473b9d4f9886973b4
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 15:28:23 -0300

    cambios

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import streamlit as st
-from pages.funciones_crud import *
+import requests
+import pandas as pd
 
-# Set page configuration
-st.set_page_config(
-    page_title="Gestion de Hoja de Calculo",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="auto",
-    show_sidebar_navigation=True  # To show the sidebar navigation
-)
+def get_data():
+        # URL de tu API de FastAPI
+    api_url = "https://python-fastapi-iamgod.koyeb.app"  # Actualiza con la URL de tu API
 
-# Explanation for each page
+    try:
+        response = requests.get(f"{api_url}/read")
+        data = response.json()
+
+        if response.status_code == 200:
+            data = data[0]
+            list_data = data['data']
+            df = pd.DataFrame(list_data)
+            return df
+        else:
+            st.error(data)
+            return None
+    except requests.RequestException as e:
+        st.error(f"Error al conectar con la API: {e}")
+        return None
+
+st.title("Bienvenido")
+
 page_explanations = {
     "Borrar": "Eliminar datos de la hoja de cálculo.",
     "Insertar": "Insertar nuevos datos en la hoja de cálculo.",
     "Modificar": "Modificar datos existentes en la hoja de cálculo."
 }
 
-selected_page = st.sidebar.radio("Seleccionar página:", list(page_explanations.keys()), index=-1)
+for page, explanation in page_explanations.items():
+    st.write(f"{page}: {explanation}")
 
-# Display explanation for selected page
-if selected_page in page_explanations:
-    st.sidebar.write(page_explanations[selected_page])
 
-# Display navigation menu
-st.sidebar.title("Navegación")
 
-# Display content for selected page
-if selected_page == "Borrar":
-    data = get_data()
-    cuerpo_insertar(data)
-elif selected_page == "Insertar":
-    st.write("You selected Insertar")
-elif selected_page == "Modificar":
-    st.write("You selected Modificar")
-# Obtener los datos de la API
+
+data = get_data()
+st.write(data)
