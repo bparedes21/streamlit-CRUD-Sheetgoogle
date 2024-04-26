357587c0a8fd0d0b6d65c9f82d8342a7a6306fda
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 19:34:37 -0300

    sr

--- main.py
+++ main.py
@@ -114,15 +114,18 @@ def insert_data(product, price, category, discount):
         # Log the error
         print(f"Error inserting data: {e}")
         return None
-
+        
 def main_sr():
-
     data = get_data()
     st.title("📝 INSERTAR Datos de la tabla Productos en Google Sheets")
 
     categories = ['Almacen', 'Mascotas', 'Bebidas y bodega']
+    category_emoji = {
+        'Almacen': '🏬',
+        'Mascotas': '🐾',
+        'Bebidas y bodega': '🍷'
+    }
 
-    # Definir los productos para cada categoría junto con sus emojis correspondientes
     productos = {
         'Almacen': [('🍝', 'Fideos'), ('🍚', 'Arroz'), ('🍅', 'Pure de Tomate')],
         'Mascotas': [('🐶', 'Alimento para perro'), ('🐱', 'Alimento para gato'), ('🐰', 'Alimento para conejo')],
@@ -130,68 +133,42 @@ def main_sr():
     }
 
     st.subheader("Ingrese los datos:")
-    # Crear menú desplegable con las categorías
     selected_category = st.selectbox("Selecciona una categoría:", categories)
 
     productos_emojis = [producto[0] + " " + producto[1] for producto in productos[selected_category]]
     productos_sin_emojis = [producto[1] for producto in productos[selected_category]]
 
-    # Crear el selectbox para los productos
     selected_productos = st.selectbox("Seleccione un producto:", productos_emojis)
 
-    # Obtener el producto sin emojis correspondiente al seleccionado
     selected_producto_sin_emojis = productos_sin_emojis[productos_emojis.index(selected_productos)]
 
     descuento = ["0", "10", "20", "30"]
     selected_descuento = st.selectbox("Seleccione un descuento:", descuento)
 
     precio = st.number_input('Ingrese un precio:', min_value=0.0, format="%.2f")
-    # Asegurarse de que el valor se trate como un flotante
     precio = round(float(precio), 2)
     precio_str = str(precio)
 
-    # Obtener emoji correspondiente a la categoría seleccionada
-    category_emoji = {
-        'Almacen': '🏬',
-        'Mascotas': '🐾',
-        'Bebidas y bodega': '🍷'
-    }
     st.subheader("Datos:")
     st.write("Producto:", selected_productos)
     st.write("Precio:", precio_str)
     st.write("Categoría:", category_emoji[selected_category], selected_category)
     st.write("Descuento:", selected_descuento)
-    if st.button("insertar"):# Verificar tipos de datos
 
-        response = insert_data(selected_producto_sin_emojis, precio_str , selected_category , selected_descuento)
-        
-        if response[1] == 200:
+    if st.button("Insertar"):
+        response = insert_data(selected_producto_sin_emojis, precio_str, selected_category, selected_descuento)
+        if response is not None and response['status_code'] == 200:
             st.empty()
             data = get_data()
             st.success("Datos insertados exitosamente")
         else:
-            st.error(f"Hubo un error al insertar los datos: {response.text}")
+            st.error("Hubo un error al insertar los datos")
 
-
-    # Mostrar los datos en Streamlit
     if data is not None and not data.empty:
         st.subheader("Tabla Productos en Google Sheets")
         st.write(data)
     else:
         st.warning("No se encontraron datos.")
-
-
-def delete_data(id):
-    # URL de la API para borrar datos
-    api_url = "https://python-fastapi-iamgod.koyeb.app/delete/"
-
-    try:
-        # Realizar la solicitud DELETE
-        response = requests.delete(api_url + str(id))
-        return response
-    except requests.RequestException as e:
-        st.error(f"Error de conexión: {e}")
-        return None
 
 def main_br():   
     # Obtener los datos de la API
