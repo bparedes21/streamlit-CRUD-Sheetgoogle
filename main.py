72720edb6fc81a8e74ab63641b17cd705ce66a96
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 15:08:17 -0300

    menu

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from .pages.funciones_crud import *
 import streamlit as st
 
-# Explanation for each page
-page_explanations = {
-    "Borrar": "Eliminar datos de la hoja de cálculo.",
-    "Insertar": "Insertar nuevos datos en la hoja de cálculo.",
-    "Modificar": "Modificar datos existentes en la hoja de cálculo."
-}
 
 # Set page configuration
 st.set_page_config(
@@ -17,20 +11,16 @@ st.set_page_config(
     show_sidebar_navigation=True  # To show the sidebar navigation
 )
 
-selected_page = st.sidebar.radio("Seleccionar página:", list(page_explanations.keys()), index=-1)
+selected_page = st.sidebar.radio("Seleccionar página:", ["Borrar", "Insertar", "Modificar"], index=-1)
 
-# Display explanation for selected page
-if selected_page in page_explanations:
-    st.sidebar.write(page_explanations[selected_page])
-
-    # Display navigation menu
-    st.sidebar.title("Navegación")
-    # Display content for selected page
-    if selected_page == "Borrar":
-        data = get_data()
-        cuerpo_insertar(data)
-    elif selected_page == "Insertar":
-        st.write("You selected Insertar")
-    elif selected_page == "Modificar":
-        st.write("You selected Modificar")
-    # Obtener los datos de la API
+# Display navigation menu
+st.sidebar.title("Navegación")
+# Display content for selected page
+if selected_page == "Borrar":
+    data = get_data()
+    cuerpo_insertar(data)
+elif selected_page == "Insertar":
+    st.write("You selected Insertar")
+elif selected_page == "Modificar":
+    st.write("You selected Modificar")
+# Obtener los datos de la API
