52aa3ac6381d989d75fbba3e1601487e2121edb3
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-25 21:22:56 -0300

    update

--- pages/modificar.py
+++ pages/modificar.py
@@ -39,55 +39,44 @@ def update_data(id_value, category, product, price, discount):
 # Obtener los datos de la API
 data = get_data()
 
-# Obtener las categorías
-opcion_crud = ['','insertar','borrar','modificar']
+# Obtener la lista de valores de la columna "ID"
+id_list = data['ID'].tolist()
+if len(id_list)!=0:
+    selected_id_list= st.selectbox("Selecciona un ID de la tabla Productos:", id_list)    
+    # Obtener las categorías
+    categories = ['Almacen','Mascotas','Bebidas y bodega']
+    productos_Almacen = ['Fideos','Arroz','Pure de Tomate']
+    productos_Mascotas = ['Alimento para perro','Alimento para gato','Alimento para conejo']
+    productos_Bebidas_y_bodega = ['Gaseosa','Agua','Vino']
 
-# Crear menú desplegable con las categorías
-selected_opcion_crud = st.selectbox("Opcion Sheet:", opcion_crud)
+    st.subheader("Ingrese los datos a modificar:")
+    # Crear menú desplegable con las categorías
+    selected_category = st.selectbox("Selecciona una categoría:", categories)
+    if selected_category == 'Almacen':
+        selected_productos = st.selectbox("Seleccione un producto:", productos_Almacen)
+    elif selected_category == 'Mascotas':
+        selected_productos = st.selectbox("Seleccione un producto:", productos_Mascotas)
+    elif selected_category == 'Bebidas y bodega':
+        selected_productos = st.selectbox("Seleccione un producto:", productos_Bebidas_y_bodega)
+    descuento = ['0','10','20','30']
+    selected_descuento = st.selectbox("Seleccione un descuento:", descuento)
+    precio = st.text_input("Ingresar precio:")
 
-# Mostrar los datos en Streamlit
-if selected_opcion_crud == 'modificar':
+    if st.button("Modificar"):
+        response = update_data(selected_id_list, selected_category, selected_productos, precio, selected_descuento)
+        if response.status_code == 200:
+            
+            st.empty()
+            data = get_data()
+            st.success("Datos modificados exitosamente")
+        else:
+            st.error(f"Hubo un error al modificar los datos")
 
-    # Obtener la lista de valores de la columna "ID"
-    id_list = data['ID'].tolist()
-    if len(id_list)!=0:
-        selected_id_list= st.selectbox("Selecciona un ID de la tabla Productos:", id_list)    
-        # Obtener las categorías
-        categories = ['Almacen','Mascotas','Bebidas y bodega']
-        productos_Almacen = ['Fideos','Arroz','Pure de Tomate']
-        productos_Mascotas = ['Alimento para perro','Alimento para gato','Alimento para conejo']
-        productos_Bebidas_y_bodega = ['Gaseosa','Agua','Vino']
+        # Limpiar el contenido actual
 
-        st.subheader("Ingrese los datos a modificar:")
-        # Crear menú desplegable con las categorías
-        selected_category = st.selectbox("Selecciona una categoría:", categories)
-        if selected_category == 'Almacen':
-            selected_productos = st.selectbox("Seleccione un producto:", productos_Almacen)
-        elif selected_category == 'Mascotas':
-            selected_productos = st.selectbox("Seleccione un producto:", productos_Mascotas)
-        elif selected_category == 'Bebidas y bodega':
-            selected_productos = st.selectbox("Seleccione un producto:", productos_Bebidas_y_bodega)
-        descuento = ['0','10','20','30']
-        selected_descuento = st.selectbox("Seleccione un descuento:", descuento)
-        precio = st.text_input("Ingresar precio:")
+else:
 
-        if st.button("Modificar"):
-            response = update_data(selected_id_list, selected_category, selected_productos, precio, selected_descuento)
-            if response.status_code == 200:
-                
-                st.empty()
-                data = get_data()
-                st.success("Datos modificados exitosamente")
-            else:
-                st.error(f"Hubo un error al modificar los datos")
-
-            # Limpiar el contenido actual
-
-    else:
-
-        st.warning("No se encontraron IDs para la categoría seleccionada.")
-
-
+    st.warning("No se encontraron IDs para la categoría seleccionada.")
 
 
 # Mostrar los datos en Streamlit
