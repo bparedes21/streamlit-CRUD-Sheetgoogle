3075cb24c93502d98f9580b5c0752174a4f3d154
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 02:03:01 -0300

    MAIN

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import streamlit as st
 
-import pages.borrar
-import pages.insertar
-import pages.modificar
-
+from pages import borrar, insertar, modificar
 # Create a sidebar with options for the pages
 selected_page = st.sidebar.radio("Seleccionar página:", ["Borrar", "Insertar", "Modificar"])
 
 
 if selected_page == "Borrar":
-    pages.borrar.show()
+    borrar.show()
 elif selected_page == "Insertar":
-    pages.insertar.show()
+    insertar.show()
 elif selected_page == "Modificar":
-    pages.modificar.show()
+    modificar.show()
