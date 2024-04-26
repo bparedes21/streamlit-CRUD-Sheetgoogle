a78be12d180cd30272620190ceede763e695ffdc
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 15:01:43 -0300

    menu true

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import streamlit as st
-
-# Set page configuration
-st.set_page_config(
-    page_title="Gestion de Hoja de Calculo",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-    show_sidebar_navigation='True'
-)
-
-# Dictionary with page names and URLs
-pages = {
-    "Borrar": "/borrar",
-    "Insertar": "/insertar",
-    "Modificar": "/modificar"
+from .pages.funciones_crud import *
+# Explanation for each page
+page_explanations = {
+    "Borrar": "Eliminar datos de la hoja de cálculo.",
+    "Insertar": "Insertar nuevos datos en la hoja de cálculo.",
+    "Modificar": "Modificar datos existentes en la hoja de cálculo."
 }
 
-# Display navigation menu
-st.sidebar.title("Navegación")
+selected_page = st.sidebar.radio("Seleccionar página:", list(page_explanations.keys()), index=-1)
+
+# Display explanation for selected page
+if selected_page in page_explanations:
+    st.sidebar.write(page_explanations[selected_page])
+
+    # Display navigation menu
+    st.sidebar.title("Navegación")
+    # Display content for selected page
+    if selected_page == "Borrar":
+        data = get_data()
+        cuerpo_insertar(data)
+    elif selected_page == "Insertar":
+        st.write("You selected Insertar")
+    elif selected_page == "Modificar":
+        st.write("You selected Modificar")
+    # Obtener los datos de la API
