d89308c0c9de5f4d0fa2160655d8c1c84e63d785
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 16:02:06 -0300

    ..

--- main.py
+++ main.py
@@ -2,26 +2,6 @@ import streamlit as st
 import requests
 import pandas as pd
 
-def get_data():
-    # URL de tu API de FastAPI
-    api_url = "https://python-fastapi-iamgod.koyeb.app"  # Actualiza con la URL de tu API
-
-    try:
-        response = requests.get(f"{api_url}/read")
-        data = response.json()
-
-        if response.status_code == 200:
-            data = data[0]
-            list_data = data['data']
-            df = pd.DataFrame(list_data)
-            return df
-        else:
-            st.error(data)
-            return None
-    except requests.RequestException as e:
-        st.error(f"Error al conectar con la API: {e}")
-        return None
-
 # Set page configuration
 st.set_page_config(
     page_title="Gestion de Hoja de Calculo",
@@ -32,9 +12,3 @@ st.set_page_config(
 )
 st.title("Bienvenido")
 
-page_explanations = {
-    "Borrar": "Eliminar datos de la hoja de cálculo.",
-    "Insertar": "Insertar nuevos datos en la hoja de cálculo.",
-    "Modificar": "Modificar datos existentes en la hoja de cálculo."
-}
-st.write(page_explanations)
