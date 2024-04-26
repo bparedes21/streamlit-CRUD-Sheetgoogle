4cbbe89c25edee0200b1198c423ff5dc4510c35c
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 14:42:51 -0300

    menu true

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ st.set_page_config(
     page_title="Gestion de Hoja de Calculo",
     page_icon="🧊",
     layout="wide",
-    initial_sidebar_state="expanded",
-    show_sidebar_navigation='False'
+    initial_sidebar_state="collapsed",
+    show_sidebar_navigation='True'
 )
 
 # Dictionary with page names and URLs
