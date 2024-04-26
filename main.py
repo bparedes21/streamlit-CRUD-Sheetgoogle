4303b3e4b52785f50fa0a2edb62df7806bf59687
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-26 15:15:27 -0300

    menu radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from .pages.funciones_crud import *
 import streamlit as st
-
+from pages.funciones_crud import *
 
 # Set page configuration
 st.set_page_config(
@@ -11,10 +10,22 @@ st.set_page_config(
     show_sidebar_navigation=True  # To show the sidebar navigation
 )
 
-selected_page = st.sidebar.radio("Seleccionar página:", ["Borrar", "Insertar", "Modificar"], index=-1)
+# Explanation for each page
+page_explanations = {
+    "Borrar": "Eliminar datos de la hoja de cálculo.",
+    "Insertar": "Insertar nuevos datos en la hoja de cálculo.",
+    "Modificar": "Modificar datos existentes en la hoja de cálculo."
+}
+
+selected_page = st.sidebar.radio("Seleccionar página:", list(page_explanations.keys()), index=-1)
+
+# Display explanation for selected page
+if selected_page in page_explanations:
+    st.sidebar.write(page_explanations[selected_page])
 
 # Display navigation menu
 st.sidebar.title("Navegación")
+
 # Display content for selected page
 if selected_page == "Borrar":
     data = get_data()
