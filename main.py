4dd3a0b3706994e7778092bbf604f5ae5737a777
bparedes21 <gestionorganizacion17@gmail.com>
2024-04-29 22:06:29 -0300

    cambiar titulo

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ def main():
 
     # Sidebar con mensaje de bienvenida
     with st.sidebar:
-        st.sidebar.title("# 👋 ¡Bienvenido/a!")
+        st.sidebar.title("👋 ¡Bienvenido/a!")
 
         st.sidebar.subheader("Navegue a través del menú:")
         page = st.sidebar.selectbox(
@@ -260,7 +260,7 @@ def main():
             "Insertar": "Insertar nuevos datos en la hoja de cálculo."
             }
 
-            st.write("👋 ¡Bienvenido/a! a mi proyecto de gestión de datos 📊")
+            st.write("#👋 ¡Bienvenido/a! a mi proyecto de gestión de datos 📊")
             st.markdown("Puedes encontrar la hoja de GOOGLE en  [Google Sheets](https://docs.google.com/spreadsheets/d/1wF_mgiNfDMFZp5M94imuFpXHdND9bvKOD41IjZNNqdo/edit?usp=sharing)")
             st.write("Este proyecto tiene como objetivo facilitar la gestión de datos de una hoja de cálculo a través de una interfaz amigable y fácil de usar.")
             for page, explanation in page_explanations.items():
